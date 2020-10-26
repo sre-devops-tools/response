d1615fa462a8c525b5f9780dfaf3506c37147a82
Damien Michau <damien@beanju.com>
2020-10-25 20:35:27 -0700

    add update in the UI

--- response/models.py
+++ response/models.py
@@ -1,9 +1,10 @@
-from .core.models import Action, Event, ExternalUser, Incident, TimelineEvent
+from .core.models import Action, Event, ExternalUser, Incident, TimelineEvent, StatusUpdate
 from .slack.models import (CommsChannel, HeadlinePost, Notification,
                            PinnedMessage, UserStats)
 
 __all__ = (
     "Action",
+    "StatusUpdate",
     "Event",
     "Incident",
     "TimelineEvent",
