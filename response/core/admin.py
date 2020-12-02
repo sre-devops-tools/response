5f324b9ea35820787c26d5849a3ff6e072e7fc8b
Damien <damien.michau@automox.com>
2020-12-02 09:28:18 -0800

    add timeline event in admin panel

--- response/core/admin.py
+++ response/core/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
 from response.core.models import (Action, Event, ExternalUser, Incident,
-                                  StatusUpdate)
+                                  StatusUpdate, TimelineEvent)
 
 admin.site.register(Action)
 admin.site.register(Event)
 admin.site.register(Incident)
 admin.site.register(ExternalUser)
 admin.site.register(StatusUpdate)
+admin.site.register(TimelineEvent)
